08528c4999531c66ea952a4a195f9c2b62578be7
michael <michael.atorres90@gmail.com>
2017-03-28 11:40:58 -0400

    added a update and delete

--- routes/hikes.js
+++ routes/hikes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 var Hike = require('../models/hike');
 
-// index authors
+// index hikes
 router.get('/', function(req, res) {
     // res.send('hikes will be here');
     Hike.find({})
@@ -18,22 +18,88 @@ router.get('/', function(req, res) {
         });
 });
 
-// new author
+// new hike
 router.get('/new', function(req, res) {
     res.render('hikes/new');
 });
 
-// show hikes
+// create hike
+router.post('/', function(req, res) {
+    var hike = new Hike({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        town: req.body.town,
+        state: req.body.state,
+        rate: req.body.rate,
+        description: req.body.description,
+        length: req.body.length
+    });
+    hike.save(function(err, hike){
+        if (err) { console.log(err); }
+
+        console.log(hike);
+        //res.send(hike);
+         res.render('hikes/show', {
+            hike: hike
+        });
+    });
+});
+  
+
+// show hike
 router.get('/:id', function(req, res) {
     Hike.findById(req.params.id)
         .exec(function(err, hike) {
             if(err) console.log(err);
 
             console.log(hike);
-            // res.send hike
+            // res.send(author);
             res.render('hikes/show', {
                 hike: hike
             });
+        });
+});
+
+// edit hike
+router.get('/:id/edit', function(req,res) {
+    Hike.findById(req.params.id)
+    .exec(function(err, hike) {
+        if (err) { console.log(err); }
+        res.render('hikes/edit', {
+            hike: hike
+        });
+    });
+});
+// update hike
+router.patch('/:id', function(req, res) {
+    Hike.findByIdAndUpdate(req.params.id, {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        town: req.body.town,
+        state: req.body.state,
+        rate: req.body.rate,
+        description: req.body.description,
+        length: req.body.length
+    }, {new: true})
+        .exec(function(err, hike) {
+            if (err) { console.log(err); }
+            console.log(hike);
+            // res.send(hike);
+            res.render('hikes/show', {
+                hike: hike
+            });
+        });
+});
+
+// delete hike
+router.delete('/:id', function(req, res) {
+    Hike.findByIdAndRemove(req.params.id)
+        .exec(function(err, hike) {
+            if (err) { console.log(err); }
+
+            console.log('Hike deleted.');
+            //res.send('Hike deleted.');  
+            res.redirect('/hikes');
         });
 });
 
