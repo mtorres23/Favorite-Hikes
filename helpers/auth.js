edabc53d9b365234308e6ce12cbf8ea7c07a20ac
michael <michael.atorres90@gmail.com>
2017-03-28 14:48:11 -0400

    added a helpers folder to start with authentication

---
+++ helpers/auth.js
@@ -0,0 +1,1 @@
+var var bcrypt = require('bcrypt')
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/signup') {
+	res.render('users/signup.hbs');
+};
+
 module.exports = router;
