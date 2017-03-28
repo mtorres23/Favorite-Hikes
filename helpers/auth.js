002153656c7f8930b1b52b2a78c944b09c046102
michael <michael.atorres90@gmail.com>
2017-03-28 17:22:49 -0400

    added a helpers/auth as well as updated my sessions

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,46 +1,45 @@
-var bcrypt = require('bcrypt');
+var bcrypt = require('bcrypt-nodejs');
 var User = require('../models/user.js');
 
 function createSecure(req, res, next) {
-  var password = req.body.password
+  var password = req.body.password;
 
   res.hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-  next()
+  next();
 }
 
 function loginUser(req, res, next) {
-  // YOU MIGHT CHANGE EMAIL TO USERNAME IF YOU DON'T WANT TO STORE EMAILS
   var email = req.body.email;
   var password = req.body.password;
 
-  var query User.findOne({ email: email }).exec()
-
-  query.then(function(foundUser){
+  User.findOne({ email: email })
+  .then(function(foundUser){
     if (foundUser == null) {
-      res.json({status: 401, data: "unauthorized"})
+      res.json({status: 401, data: "unauthorized"});
 
     } else if (bcrypt.compareSync(password, foundUser.password_digest)) {
       req.session.currentUser = foundUser;
     }
-    next()
+    next();
   })
   .catch(function(err){
-    res.json({status: 500, data: err})
+    res.json({status: 500, data: err});
   });
 }
 
-function authorize(req, res, next) {
-  var currentUser = req.session.currentUser;
-
-  if (!currentUser || currentUser._id !== req.params.id ) {
-    res.json({status: 401, data: 'unauthorized'});
-  } else {
-    next();
+function authorized(req, res, next) {
+  console.log(req.session.currentUser);
+  console.log(req.params.id)
+  if (!req.session.currentUser || req.session.currentUser._id !== req.params.id) {
+    return res.json({status: 404, data: "unauthorized"});
   }
+  next();
 };
+
+//Export this function below:
 
 module.exports = {
   createSecure: createSecure,
   loginUser: loginUser,
-  authorize: authorize
-}
+  authorized: authorized
+};
