c7e7045d06403557cb9e9cd3aedb89dca7b8d1f5
michael <michael.atorres90@gmail.com>
2017-03-28 17:51:51 -0400

    added a userschema

--- models/hike.js
+++ models/hike.js
@@ -27,5 +27,24 @@ HikeSchema.virtual('fullName').get(function () {
     return this.first_name + ' ' + this.last_name;
 });
 
+var UserSchema = new Schema({
+  username: String,
+  email: String,
+  password_digest: String,
+  list: [ListSchema],
+  created_at: Date,
+  updated_at: Date
+});
+
+
+UserSchema.pre('save', function(next) {
+    now = new Data();
+    this.updated_at = now;
+    if ( !this.created_at ) {
+        this.created_at = now;
+    }
+    next();
+});
+
 
 module.exports = mongoose.model("Hike", HikeSchema);
