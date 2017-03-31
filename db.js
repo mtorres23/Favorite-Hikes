c73f00bfa76a6ddbaa4e2369c67fef6f12e837ad
michael <michael.atorres90@gmail.com>
2017-03-30 20:13:18 -0400

    cammented out var db = mongoose.connection; and added an if else for mongo db to try and fix database

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 var mongoose = require('mongoose');
 
-var db = mongoose.connection;
+//var db = mongoose.connection;
 
-// if(process.env.MONGODB_URI) {
-//   mongoose.connect(process.env.MONGODB_URI);  
-// } else {
-//   mongoose.connect('mongodb://localhost/Favorite-Hikes');
-// }
+if(process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);  
+} else {
+  mongoose.connect('mongodb://localhost/Favorite-Hikes');
+}
 
  mongoose.promise = require('bluebird');
 
