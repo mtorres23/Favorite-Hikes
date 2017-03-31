268e5c17a45f903f10c950e36b0241aecdf8809f
michael <michael.atorres90@gmail.com>
2017-03-31 15:12:48 -0400

    finally figure out my issue. In my layout.hbs i had set the href to local host instead of routing it to the correct path.

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-// uncomment after placing favicon in /public
+
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
