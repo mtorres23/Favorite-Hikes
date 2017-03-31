d685c99e39684d5ac9878bef7ffc48e1c1698787
michael <michael.atorres90@gmail.com>
2017-03-31 13:58:13 -0400

    commented out homepage in index.js

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Take A Hike' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Take A Hike' });
+// });
 
 module.exports = router;
